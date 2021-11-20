8418b489a86648220e133cdfc880fb092a939429
Kinshuk Kishore <kinshukkishore1.10@gmail.com>
2021-11-20 23:51:42 +0530

    Added build folder

---
+++ build/precache-manifest.d9dfb71b7157b47567da1bf68500bfbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2204d746b6eb2a1f9293f024cf23ae14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9d8adca62b32703c05c",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9d8adca62b32703c05c",
+    "url": "/static/js/main.d2307288.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
